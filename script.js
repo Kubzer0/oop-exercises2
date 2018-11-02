be557ef47ea9f9120d583856b1f73d76832601b5
kubzer0 <jakub.gryka@gmail.com>
2018-11-02 11:06:25 +0100

    toggle added, render and init function

--- script.js
+++ script.js
@@ -1,34 +1,50 @@
-const SideBarMenu = function () {
+const SideBarMenu = function (color, direction) {
     this.isOpen = true
-    this.isOnTheLeft = true
-    this.isOnTheRight = false
-    this.bgColor = 'red'
+    this.isOnTheLeft = direction === 'left' ? true : false
+    this.isOnTheRight = direction === 'right' ? true : false
+    this.bgColor = color || 'red'
+    this.menuDiv = null
+    if (!this.isOnTheLeft && !this.isOnTheRight) {
+        this.isOnTheLeft = true
+    }
+    this.init()
+}
+
+SideBarMenu.prototype.init = function () {
+    this.render()
+}
+
+SideBarMenu.prototype.render = function () {
+    if (this.menuDiv) {this.menuDiv.remove()}
+    this.menuDiv = document.createElement('div')
+
+    this.menuDiv.style.backgroundColor = this.bgColor
+    this.menuDiv.style.width = '200px'
+    this.menuDiv.style.height = '100vh'
+    this.menuDiv.style.top = '0px'
+    this.menuDiv.style.position = 'fixed'
+    if (this.isOnTheLeft) {
+        this.menuDiv.style.left = '0'
+    }
+
+    if (this.isOnTheRight) {
+        this.menuDiv.style.right = '0'
+    }
+
+    if (!this.isOpen) {
+        this.menuDiv.style.display = 'none'
+    }
+
+    document.body.appendChild(this.menuDiv)
 }
 
 
-
-SideBarMenu.prototype.render = function () {
-    const menuDiv = document.createElement('div')
-
-    menuDiv.style.backgroundColor = this.bgColor
-    menuDiv.style.width = '200px'
-    menuDiv.style.height = '100vh'
-    menuDiv.style.top = '0px'
-    menuDiv.style.position = 'fixed'
-    if (this.isOnTheLeft){
-        menuDiv.style.left = '0'
-    }
-
-    if (this.isOnTheRight){
-        menuDiv.style.right = '0'
-    }
-
-    if (!this.isOpen){
-        menuDiv.style.display = 'none'
-    }
-
-    document.body.appendChild(menuDiv)
+SideBarMenu.prototype.toggleMenu = function () {
+    if (this.isOpen === true) { this.isOpen = false }
+    if (!this.isOpen === false) { this.isOpen = true }
+    this.render()
 }
 
 const menu1 = new SideBarMenu()
-menu1.render()
+const menu2 = new SideBarMenu('green', 'right')
+menu2.toggleMenu()
