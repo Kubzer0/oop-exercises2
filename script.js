fa5af22cafc72a2e8d94255a2a1fbacc0f384109
kubzer0 <jakub.gryka@gmail.com>
2018-11-02 10:06:53 +0100

    styling added to render

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-const SideBarMenu = function(){
+const SideBarMenu = function () {
     this.isOpen = true
     this.isOnTheLeft = true
     this.isOnTheRight = false
     this.bgColor = 'red'
+}
 
+
+
+SideBarMenu.prototype.render = function () {
+    const menuDiv = document.createElement('div')
+
+    menuDiv.style.backgroundColor = this.bgColor
+    menuDiv.style.width = '200px'
+    menuDiv.style.height = '100vh'
+    menuDiv.style.top = '0px'
+    menuDiv.style.position = 'fixed'
+    if (this.isOnTheLeft){
+        menuDiv.style.left = '0'
+    }
+
+    if (this.isOnTheRight){
+        menuDiv.style.right = '0'
+    }
+
+    if (!this.isOpen){
+        menuDiv.style.display = 'none'
+    }
+
+    document.body.appendChild(menuDiv)
 }
 
 const menu1 = new SideBarMenu()
+menu1.render()
